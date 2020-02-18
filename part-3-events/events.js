2fb6141c829249e03dd3f601a0c8932ea604d157
monstaro <monstaro32@gmail.com>
2020-02-17 17:08:56 -0700

    Removes unnecessary colons on input selector

--- part-3-events/events.js
+++ part-3-events/events.js
@@ -8,18 +8,21 @@ $('button').on('click', function(e){
 )
 
 $('#clear-btn').on('click', function() {
-  $('input').val('')
+  $('input').val('');
+  localStorage.clear()
 })
 
-$(':input').on('change', function() {
-  for (var i = 0; i < ($(':input').length); i++) {
-    localStorage.setItem('input'+i, $(':input').eq(i).val())
+console.log($('input'))
+
+$('input').on('change', function() {
+  for (var i = 0; i < ($('input').length); i++) {
+    localStorage.setItem('input'+i, $('input').eq(i).val())
   }
 })
 
 $(document).ready(function() {
-  for (var i = 0; i < ($(':input').length); i++) {
-    $(':input').eq(i).val(localStorage.getItem('input'+i))
+  for (var i = 0; i < ($('input').length); i++) {
+    $('input').eq(i).val(localStorage.getItem('input'+i))
   }
 })
 
