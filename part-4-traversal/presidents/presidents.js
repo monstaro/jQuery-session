cbfe0b962e2278a4d8c7a679d1169de1c7fec6c5
monstaro <monstaro32@gmail.com>
2020-02-17 20:47:20 -0700

    Begins exercises

--- part-4-traversal/presidents/presidents.js
+++ part-4-traversal/presidents/presidents.js
@@ -1,1 +1,7 @@
-const $ = require('jquery');
+let tr = $('tr')
+
+$('.name').css('background-color', 'red');
+$('.term').css('background-color', 'white');
+$('.party').css('background-color', 'blue');
+
+$('input[type="checkbox"]').css('background-color', 'blue')
